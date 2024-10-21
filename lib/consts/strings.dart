8dfa24f66eea93bd6e978026118ecd14432ec58d
qasiagul <qasiagul633@gmail.com>
2024-10-21 17:04:37 +0500

    first commit

---
+++ lib/consts/strings.dart
@@ -0,0 +1,4 @@
+const String degree = "°";
+const String apiKey = "24eeaf148f98a44fea21036fe233bd52";
+const String currentEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
